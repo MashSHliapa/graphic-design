d1a0fae9da715ac329f41370ea28fed5bf3ba368
@MashSHliapa <shliapa.maria@mail.ru>
2023-12-02 00:55:50 +0300

    feat (project): create page Cutaway

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,14 +4,16 @@ import { Main } from '../Main'
 import { Footer } from '../Footer'
 import './Layout.scss'
 import { Flyers } from '../../pages/Flyers'
+import { Cutaway } from '../../pages/Сutaway'
 
 export function Layout() {
   return (
     <div className="layout">
-      <Header/>
-      <Main/>
-      <Flyers/>
-      <Footer/>
+      <Header />
+      <Main />
+      <Flyers />
+      <Cutaway />
+      <Footer />
     </div>
   )
 }
