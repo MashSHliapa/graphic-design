855b3832b594f1420d7d90e464136dc9922c2cfe
@MashSHliapa <shliapa.maria@mail.ru>
2024-01-04 23:16:16 +0300

    feat(project): add adaptive for MainTheme, Price, Services

--- src/components/GoToMain/index.tsx
+++ src/components/GoToMain/index.tsx
@@ -4,10 +4,12 @@ import './GoToMain.scss'
 
 export function GoToMain() {
   return (
-    <div className="go-to-main__body">
-      <img src={to_main} alt="to_main" />
-      <div className="go-to-main__text">
-        <NavLink to="/" target="_top">на главную</NavLink>
+    <div className="go-to-main">
+      <div className="go-to-main__body">
+        <img src={to_main} alt="to_main" />
+        <div className="go-to-main__text">
+          <NavLink to="/" target="_top">на главную</NavLink>
+        </div>
       </div>
     </div>
   )
--- src/components/Price/index.tsx
+++ src/components/Price/index.tsx
@@ -18,27 +18,27 @@ export function Price() {
 
             <li className="menu-main__item menu-main__item_price">
               <span>визитки</span>
-              <span className="menu-main__price">от 15$</span>
+              <span className="menu-main__price">от 5$</span>
             </li>
 
             <li className="menu-main__item menu-main__item_price">
               <span>блокноты</span>
-              <span className="menu-main__price">от 15$</span>
+              <span className="menu-main__price">от 18$</span>
             </li>
 
             <li className="menu-main__item menu-main__item_price">
               <span>МЕНЮ</span>
-              <span className="menu-main__price">от 15$</span>
+              <span className="menu-main__price">от 20$</span>
             </li>
 
             <li className="menu-main__item menu-main__item_price">
               <span>ДИПЛОМЫ, БЛАГОДАРНОСТИ</span>
-              <span className="menu-main__price">от 15$</span>
+              <span className="menu-main__price">от 8$</span>
             </li>
 
             <li className="menu-main__item menu-main__item_price">
               <span>СЕРТИФИКАТЫ, Пригласительные</span>
-              <span className="menu-main__price">от 15$</span>
+              <span className="menu-main__price">от 8$</span>
             </li>
 
             <li className="menu-main__item menu-main__item_price">
@@ -48,27 +48,26 @@ export function Price() {
 
             <li className="menu-main__item menu-main__item_price">
               <span>БАННЕРЫ</span>
-              <span className="menu-main__price">от 15$</span>
+              <span className="menu-main__price">от 16$</span>
             </li>
 
             <li className="menu-main__item menu-main__item_price">
               <span>СТИКЕРЫ,БИРКИ</span>
-              <span className="menu-main__price">от 15$</span>
+              <span className="menu-main__price">от 3$</span>
             </li>
 
             <li className="menu-main__item menu-main__item_price">
               <span>ПОСТ & СТОРИС</span>
-              <span className="menu-main__price">от 15$</span>
+              <span className="menu-main__price">от 17$</span>
             </li>
 
             <li className="menu-main__item menu-main__item_price">
               <span>ЛОГОТИПЫ</span>
-              <span className="menu-main__price">от 15$</span>
+              <span className="menu-main__price">от 30$</span>
             </li>
           </ul>
         </div>
       </div>
-
     </MainTheme>
   )
 }
